0b94f0b6751a6f7c9c35cdba72b0e8b7d849ccb9
Đỗ Huy Hoàng <huyhoang14901@gmail.com>
2022-01-11 23:06:37 +0700

    test

---
+++ app/src/androidTest/java/com/huyhoang/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,24 @@
+package com.huyhoang.affirmations
+
+import android.content.Context
+import com.example.affirmations.adapter.ItemAdapter
+import com.huyhoang.affirmations.model.Affirmation
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito
+
+class AffirmationsAdapterTests {
+    private val context = Mockito.mock(Context::class.java)
+
+    @Test
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+        )
+
+        val adapter = ItemAdapter(context, data)
+        Assert.assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+
+}
